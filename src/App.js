6cd14236d5a6af709c50584a539d65b601485887
omaomach <aaronjoash44@gmail.com>
2023-04-01 23:51:42 +0300

    called components in App.js instead of Index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import '../src/styles/main.css';
+import FindArtist from './components/FindArtist';
+import Footer from './components/Footer';
+import Landing from './components/Landing'
 
 function App() {
   return (
     <div>
-
+      {/* <Landing /> */}
+      {/* <Footer /> */}
+      <FindArtist />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './components/Navbar';
-import Landing from './components/Landing';
-import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Navbar /> */}
-    {/* <Landing /> */}
-    <Footer />
+    <App />
   </React.StrictMode>
 );
 
