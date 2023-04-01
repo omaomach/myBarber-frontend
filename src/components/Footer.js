7ccbeb4c8950a63d6d7de8983ecc5b278ff28654
Vincent <makokhavomondi@gmail.com>
2023-04-01 14:51:14 +0300

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import logo from '../assets/logo.png'
 function Footer() {
     return (
         <div id="footer">
-            <div className="footer-outer">
+            <div id="footer-logo-item" className="footer-item">
                 <div id="footer-logo">
                     <div id="img-wrapper">
                         <img id="footer-log-img" src={logo} alt="company logo"/>
@@ -17,7 +17,7 @@ function Footer() {
                 </div>
             </div>
 
-            <div id="footer-quick-links" className="footer-inner">
+            <div id="footer-quick-links-item" className="footer-item">
                 <h1>Quick Links</h1>
                 <ul>
                     <li>Contact Us</li>
@@ -27,15 +27,16 @@ function Footer() {
                 </ul>
             </div>
 
-            <div id="footer-contact-info" className="footer-inner">
+            <div id="footer-contact-info-item" className="footer-item">
                 <h1>Contact Info</h1>
                 <ul>
-                    <li>info@inkmasters.co.ke</li>
-                    <li>0724252552</li>
+                    <li>email: info@inkmasters.co.ke</li>
+                    <li>tel: 0724252552</li>
                 </ul>
             </div>
 
-            <div id="footer-newsletter" className="footer-outer">
+            <div id="footer-newsletter-item" className="footer-item">
+                <h1>Newsletter</h1>
                 <form id="footer-newsletter-form">
                     <input type="email" />
                     <input type="submit"/>
