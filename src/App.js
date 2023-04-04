426e461fa1f406377a10ae703579206ead47eac3
omaomach <aaronjoash44@gmail.com>
2023-04-04 16:14:58 +0300

    feat: created find an artist page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       {/* <Navbar /> */}
       {/* <Landing /> */}
       {/* <Footer /> */}
-      {/* <FindArtist /> */}
+      <FindArtist />
     </div>
   );
 }
--- src/components/FindArtist.js
+++ src/components/FindArtist.js
@@ -11,7 +11,48 @@ function FindArtist() {
 
                 </div>
                 <div id="filter-buttons">
+                    <button id="first-location-button"><p>Location</p></button>
+                    <button className="location-buttons"><p>Your Location</p></button>
+                    <button className="location-buttons"><p>Studio Name</p></button>
+                    <button className="location-buttons"><p>Artist Name</p></button>
+                </div>
+                <div id="featured-studios">
+                    <h2>Featured Studios</h2>
+                    <button id="featured-studios-left-button">Left</button>
+                    <button id="featured-studios-right-button">Right</button>
+                </div>
+                <div id="featured-studios-cards">
+                    <div className="studio-card">
 
+                    </div>
+                    <div className="studio-card">
+                        
+                    </div>
+                    <div className="studio-card">
+                        
+                    </div>
+                    <div className="studio-card">
+                        
+                    </div>
+                </div>
+                <div id="featured-artists">
+                    <button id="artist-filter-button"><p>Featured Artists</p></button>
+                    <button id="featured-artists-left-button">Left</button>
+                    <button id="featured-artists-right-button">Right</button>
+                </div>
+                <div id="featured-artists-cards">
+                    <div className="artist-card">
+
+                    </div>
+                    <div className="artist-card">
+                        
+                    </div>
+                    <div className="artist-card">
+                        
+                    </div>
+                    <div className="artist-card">
+                        
+                    </div>
                 </div>
             </div>
         </div>
