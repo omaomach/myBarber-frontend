ded5e93afb7a714fff63334d7a350e86ab9cad09
omaomach <aaronjoash44@gmail.com>
2023-04-05 09:06:49 +0300

    created left and right artist page components

--- src/components/ArtistPage.js
+++ src/components/ArtistPage.js
@@ -1,9 +1,19 @@
 import React from "react";
 import '../styles/artistpage.css'
+import Navbar from "./Navbar";
 
 function ArtistPage() {
     return (
-        <div>
+        <div id="parent-div">
+            <Navbar />
+            <div id="secondary-div">
+                <div id="artist-details-div">
+
+                </div>
+                <div id="artist-work-images">
+
+                </div>
+            </div>
 
         </div>
     )
