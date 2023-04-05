72cf4ec0501ae439b82922dd58325728e9d7f8a8
omaomach <aaronjoash44@gmail.com>
2023-04-05 16:17:05 +0300

    added option buttons to ArtistPage component

--- src/components/ArtistPage.js
+++ src/components/ArtistPage.js
@@ -8,7 +8,19 @@ function ArtistPage() {
             <Navbar />
             <div id="secondary-div">
                 <div id="artist-details-div">
-
+                    <div id="image-name-div">
+                        <div id="image">
+                        </div>
+                        <div id="name">
+                            <p>Joash Omao Machoka</p>
+                            <p>Status: Available</p>
+                        </div>
+                    </div>
+                    <div id="artist-option-buttons">
+                        <button id="option-button-1">Book Artist</button>
+                        <button className="option-buttons">Review Artist</button>
+                        <button className="option-buttons">Report Artist</button>
+                    </div>
                 </div>
                 <div id="artist-work-images">
 
