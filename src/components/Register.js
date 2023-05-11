52836e45f8793403b808bf76ca78b46ba621bd27
omaomach <aaronjoash44@gmail.com>
2023-05-11 20:17:30 +0300

    added registration form

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,12 +8,20 @@ function Register() {
 
       </div>
       <div id="right-side">
-        <h1>Get started</h1>
-        <h3>Create your account now</h3>
+        <h1 id="register-h1">Get started</h1>
+        <h3 id="create-your-account-now">Create your account now</h3>
         <div id="field-container">
-            
-
+            <form>
+                <h4>Username</h4>
+                <input type="text" id="username"/>
+                <h4>Email</h4>
+                <input type="email" id="client-email"/>
+                <h4>Password</h4>
+                <input type="password" id="client-password"/>
+                <button id="register-button" type="submit">Sign Up</button>
+            </form>
         </div>
+        <h3 id="have-an-account">Have an account? Login</h3>
       </div>
     </div>
   );
