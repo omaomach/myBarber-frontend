8433dac309f334eb8125134c65222f70d406262b
omaomach <aaronjoash44@gmail.com>
2023-06-22 18:09:06 +0300

    working or register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,30 +1,35 @@
-// import React from "react";
-// import "../styles/register.css";
+import React from "react";
+import "../styles/register.css";
 
-// function Register() {
-//   return (
-//     <div id="register-page-parent">
-//       <div id="left-side">
+function Register() {
+  return (
+    <div id="register-page-parent">
+      <div id="left-side">
 
-//       </div>
-//       <div id="right-side">
-//         <h1 id="register-h1">Get started</h1>
-//         <h3 id="create-your-account-now">Create your account now</h3>
-//         <div id="field-container">
-//             <form>
-//                 <h4>Username</h4>
-//                 <input type="text" id="username"/>
-//                 <h4>Email</h4>
-//                 <input type="email" id="client-email"/>
-//                 <h4>Password</h4>
-//                 <input type="password" id="client-password"/>
-//                 <button id="register-button" type="submit">Sign Up</button>
-//             </form>
-//         </div>
-//         <h3 id="have-an-account">Have an account? Login</h3>
-//       </div>
-//     </div>
-//   );
-// }
+      </div>
+      <div id="right-side">
+        <h1 id="register-h1">myBarber</h1>
+        <h2 id="create-your-account-now">Register and enjoy all our services</h2>
+        <div className="field-container">
+            <form>
+                <input type="text" id="username" placeholder="Username"/>
+                <input type="email" id="client-email" placeholder="Email"/>
+                <input type="phone_number" id="phone_number" placeholder="Phone Number"/>
+                <input type="password" id="client-password" placeholder="Password"/>
+                <input type="password" id="client-password" placeholder="Password Confirmation"/>
+                <button id="register-button" type="submit">Sign Up</button>
+            </form>
+            
+        </div>
+        <div className="straight-lines">
+            <div style={{ borderTop: "2px solid #fff ", marginLeft: 20, marginRight: 20, width: 210 }}></div>
+            <p id="straights-line-p">or</p>
+            <div style={{ borderTop: "2px solid #fff ", marginLeft: 20, marginRight: 20, width: 210 }}></div>
+        </div>
+        <h3 id="have-an-account">Have an account? Login</h3>
+      </div>
+    </div>
+  );
+}
 
-// export default Register;
+export default Register;
