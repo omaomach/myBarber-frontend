9740bb3afea4c50294bdd49e7a87984bf9d5f2cd
omaomach <aaronjoash44@gmail.com>
2023-09-25 22:37:47 +0300

    working on single image

--- src/components/SingleImage.js
+++ src/components/SingleImage.js
@@ -1,10 +1,23 @@
 import React from "react";
 import '../styles/singleimage.css'
+import logo from '../assets/logo.png'
+import menu from '../assets/menu.png'
 
 function SingleImage() {
     return (
         <div>
+        <div className="navbar">
+                <img src={logo} alt="logo" className="logo"/>
+                <nav>
+                    <ul id="menuList">
+                        <li><a href="#">Find an Artist</a></li>
+                        <li><a href="#">Explore Tattoos</a></li>
+                        <li><a href="#">Learn More About Tattoos</a></li>
+                    </ul>
+                </nav>
+                <img src={menu} alt="menu" className="menu"/>
 
+            </div>
         </div>
     )
 }
