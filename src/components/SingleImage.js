126d16405f595031e31b1c8b28953bc0ae6ef342
omaomach <aaronjoash44@gmail.com>
2023-09-26 23:14:00 +0300

    removed code from SingleImage component

--- src/components/SingleImage.js
+++ src/components/SingleImage.js
@@ -6,18 +6,7 @@ import menu from '../assets/menu.png'
 function SingleImage() {
     return (
         <div>
-        <div className="navbar">
-                <img src={logo} alt="logo" className="logo"/>
-                <nav>
-                    <ul id="menuList">
-                        <li><a href="#">Find an Artist</a></li>
-                        <li><a href="#">Explore Tattoos</a></li>
-                        <li><a href="#">Learn More About Tattoos</a></li>
-                    </ul>
-                </nav>
-                <img src={menu} alt="menu" className="menu"/>
-
-            </div>
+        
         </div>
     )
 }
